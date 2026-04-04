930d579ac31a184528265ee4225859163fa9c0a5
Haziq Danial <haziqdanial031@gmail.com>
2026-04-05 02:44:37 +0800

    added primevue & primeicons

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,58 @@ import { createInertiaApp, Head, Link } from '@inertiajs/vue3'
 import Layouts from './Pages/Layouts/layouts.vue';
 import { route, ZiggyVue } from 'ziggy-js';
 
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura'
+import { definePreset } from '@primeuix/themes';
+
+const Noir = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{zinc.50}',
+            100: '{zinc.100}',
+            200: '{zinc.200}',
+            300: '{zinc.300}',
+            400: '{zinc.400}',
+            500: '{zinc.500}',
+            600: '{zinc.600}',
+            700: '{zinc.700}',
+            800: '{zinc.800}',
+            900: '{zinc.900}',
+            950: '{zinc.950}'
+        },
+        colorScheme: {
+            light: {
+                primary: {
+                    color: '{zinc.950}',
+                    inverseColor: '#ffffff',
+                    hoverColor: '{zinc.900}',
+                    activeColor: '{zinc.800}'
+                },
+                highlight: {
+                    background: '{zinc.950}',
+                    focusBackground: '{zinc.700}',
+                    color: '#ffffff',
+                    focusColor: '#ffffff'
+                }
+            },
+            dark: {
+                primary: {
+                    color: '{zinc.50}',
+                    inverseColor: '{zinc.950}',
+                    hoverColor: '{zinc.100}',
+                    activeColor: '{zinc.200}'
+                },
+                highlight: {
+                    background: 'rgba(250, 250, 250, .16)',
+                    focusBackground: 'rgba(250, 250, 250, .24)',
+                    color: 'rgba(255,255,255,.87)',
+                    focusColor: 'rgba(255,255,255,.87)'
+                }
+            }
+        }
+    }
+});
+
 createInertiaApp({
     title: (title) => `PETAKOM ${title}`,
     resolve: name => {
@@ -20,6 +72,14 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Noir,
+                    options: {
+                        darkModeSelector: false
+                    }
+                }
+            })
             .component("Head", Head)
             .component("Link", Link)
             .component("route", route)
